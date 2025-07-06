2cda770d2cdf2f6ebba60fe9d8a7230168c907d8
Kris Tokarz <kj.tokarz@gmail.com>
2025-07-06 11:39:13 -0400

    Figure out how to create a reactive file
    After much troubleshooting I have figured out the correct way to build a
    reactive file using an html file. This commit uses the example provided
    by the framework authors. For now, I am going to commit this as a
    working sample and iterate on it with the image manipulation idea.

--- ImageManip/app/resources/images/ImagesController.jl
+++ ImageManip/app/resources/images/ImagesController.jl
@@ -1,3 +1,14 @@
 module ImagesController
-  # Build something great
+using Genie.Renderer.Html, GenieFramework
+@genietools
+
+@app begin
+  @in N = 100
+  @out msg = ""
+  @onchange N begin
+    msg = "N = $N"
+  end
+end
+
+@page("/images", "./app/resources/images/views/imageAsAMatrix.jl.html")
 end
